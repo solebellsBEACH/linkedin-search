d3990ab1303be7fe133fd2dd3b47c5f7e6775faa
solebellsBEACH <lucassxxavier@gmail.com>
2025-06-17 21:31:06 -0300

    feat: adding design tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
+import { useState } from "react";
 import { DevelopersTab } from "./tabs/developers";
+import { QaTab } from "./tabs/qa-tab";
+import { DesignTab } from "./tabs/design-tab";
 import './App.css'
 
 export default function Popup() {
+  const [activeTab, setActiveTab] = useState("developers");
+
+  const renderTab = () => {
+    switch (activeTab) {
+      case "developers":
+        return <DevelopersTab />;
+      case "qa":
+        return <QaTab />;
+      case "design":
+        return <DesignTab />;
+      default:
+        return <DevelopersTab />;
+    }
+  };
 
   return (
     <div className="container">
       <h3 className="title">🔎 Busca no LinkedIn</h3>
-      <DevelopersTab/>
+      <nav>
+        <button onClick={() => setActiveTab("developers")} className={activeTab === "developers" ? "active" : ""}>Desenvolvedores</button>
+        <button onClick={() => setActiveTab("qa")} className={activeTab === "qa" ? "active" : ""}>QA</button>
+        <button onClick={() => setActiveTab("design")} className={activeTab === "design" ? "active" : ""}>Design</button>
+      </nav>
+
+      <section>
+        {renderTab()}
+      </section>
     </div>
-  )
+  );
 }
